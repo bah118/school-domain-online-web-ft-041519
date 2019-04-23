8b845e2f383a27bb6b02cb525bfb11548f94cb4b
BethAnn Hanlon <herbivore137@gmail.com>
2019-04-23 00:44:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+class School 
+  
+  attr_reader :school_name, :roster
+  
+  ROSTER = {}
+  
+  def initialize(school_name)
+    @school_name = school_name
+  end
+  
+  def roster
+    ROSTER = {}
+  end
+  
+  def add_student(student_name, grade_level)
+   # @roster[grade_level] = student_name
+  
+   
+   if @roster.has_key?(grade_level)
+     @roster[grade_level] << student_name
+     
+   else
+     @roster[grade_level] = []
+     @roster[grade_level] << student_name
+   end
+   
+   
+   
+  # @roster.each do |grade, name|
+  #   if @roster.has_key(grade)
+  #     @roster[grade] << name
+  #   end
+  # end
+  end
+  
+  
+end
